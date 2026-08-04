6d7b656c0ca71d38f50b30d093d03701941e37dc
Donna Zavala Perez <donna.fearles@gmail.com>
2026-08-04 11:10:52 -0500

    fixin  vertex

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'SNryMe5RuEVNnpzqShOKy17wCaDz-QZp',
+            'X-App-Proxy': 'XZZa8jMbTzDw13sT3T9G6NxKAvlxWizF',
           },
           body: JSON.stringify(requestDetails),
         };
